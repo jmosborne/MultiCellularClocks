d173a408776ba0386f3f03103f668cea86f9b3fe
jmosborne <jmosborne@unimelb.edu.au>
2026-06-03 11:23:39 +1000

    Example sims

--- matlab/PlotNumCells.m
+++ matlab/PlotNumCells.m
@@ -20,8 +20,8 @@ for case_index = 1:size(cell_numbers,1)
         data{i} = load([base_dir,'Case', num2str(case_index-1),'_Cycling', num2str(cell_numbers(case_index,1)),'_NonCycling',num2str(cell_numbers(case_index,2)), '/', num2str(i-1),'/results_from_time_0/results.non_cycling_cell_counts'])
 
         
-        plot(data{i}(:,1),data{i}(:,2),LineWidth=0.1, LineStyle=":", Color='r');
-        plot(data{i}(:,1),data{i}(:,3),LineWidth=0.1, LineStyle=":", Color='b');
+        plot(data{i}(:,1),data{i}(:,2),'LineWidth',0.2,'LineStyle',':','Color',[1 0.7 0.7]);
+        plot(data{i}(:,1),data{i}(:,3),'LineWidth',0.2,'LineStyle',':','Color',[0.7 0.7 1]);
 
         if i==1
             mean_data = data{i};
@@ -36,13 +36,15 @@ for case_index = 1:size(cell_numbers,1)
 
     % Plot the loaded data
 
-    P1 = plot(mean_data(:,1),mean_data(:,2),'r',LineWidth=2.0);
-    P2 = plot(mean_data(:,1),mean_data(:,3),'b',LineWidth=2.0);
+    total_mean = mean_data(:,2) + mean_data(:,3);
+    P0 = plot(mean_data(:,1),total_mean,'k','LineWidth',6.0);
+    P1 = plot(mean_data(:,1),mean_data(:,2),'r','LineWidth',6.0);
+    P2 = plot(mean_data(:,1),mean_data(:,3),'b','LineWidth',6.0);
     xlabel('Time');
     ylabel('Cell Counts');
     ylim([0,600]);
     title('Mean Cell Counts Over Time');
-    legend([P1,P2],{'Non Cycing','Cycling'},Location="northwest")
+    legend([P0,P1,P2],{'Total','Non Cycing','Cycling'},'Location','northwest')
     SaveAsPngEpsAndFig(-1,['ClocksMonolayer_Cycling',num2str(cell_numbers(case_index,1)),'_NonCycling',num2str(cell_numbers(case_index,2))], 12, 7/5, 12)
 
 end
